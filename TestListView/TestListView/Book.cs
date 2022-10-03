0126cf66c3412df3149c92a66c98d98da72c6556
Egraf505 <egor.leskov.03@mail.ru>
2022-10-03 11:12:34 +0300

    Добавьте файлы проекта.

---
+++ TestListView/TestListView/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestListView
+{
+    public class Book
+    {
+        private string _name;
+        public string Name { get { return _name; }}
+
+        private string _autor;
+        public string Autor { get { return _autor; }}
+
+        public Book(string name, string autor)
+        {
+            _name = name;
+            _autor = autor; 
+        }
+
+        public override string ToString()
+        {
+            return $"Название книги: {_name} Автор: {_autor}";
+        }
+    }
+}
---
+++ TestListView/TestListView/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestListView
+{
+    public partial class MainPage : ContentPage
+    {
+        List<Book> selectedBooks = new List<Book>();
+
+        List<Book> books = new List<Book>() {
+                new Book("1231","Толстой"),
+                new Book("1231","Пущкин"),
+                new Book("1231","Гоголь"),
+                new Book("12","Толстой")
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();            
+
+            ListOfBooks.ItemsSource = books;
+        }
+
+        private void ListOfBooks_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item != null)
+            {
+                selectedBooks.Add((Book)e.Item);                
+                CountOfBooks.Text = selectedBooks.Count.ToString();
+            }
+            ((ListView)sender).SelectedItem = null;
+        }
+
+        private async void GoToAPge_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SeelctBooks(selectedBooks));
+        }
+    }
+}
